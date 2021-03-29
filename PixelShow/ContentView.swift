2fb10934ad4875e5f0743c521dd42bb6ec4de925
Marina De Pazzi <pazzi.dev@gmail.com>
2021-03-29 16:01:49 -0300

    chore: creating base project

---
+++ PixelShow/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  PixelShow
+//
+//  Created by Marina De Pazzi on 29/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PixelShow/PixelShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelShowApp.swift
+//  PixelShow
+//
+//  Created by Marina De Pazzi on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct PixelShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
