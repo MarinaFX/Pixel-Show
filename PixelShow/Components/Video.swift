84a9beba67e7d81c2b617fd4ed467d867e513ba7
Thaís Fernandes <throdriguesfernandes@gmail.com>
2021-03-31 17:15:26 -0300

    done

---
+++ PixelShow/Components/Video.swift
@@ -0,0 +1,26 @@
+//
+//  Video.swift
+//  PixelShow
+//
+//  Created by Thaís Fernandes on 31/03/21.
+//
+
+import SwiftUI
+import AVKit
+
+struct Video: View {
+    var body: some View {
+        VideoPlayer(player: AVPlayer(url:  URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4")!))
+        .frame(maxWidth: .infinity, maxHeight: 200)
+        .background(Color.gray)
+        .cornerRadius(5.0)
+        .padding(.top)
+        .padding(.bottom, 30)
+    }
+}
+
+struct Video_Previews: PreviewProvider {
+    static var previews: some View {
+        Video()
+    }
+}
