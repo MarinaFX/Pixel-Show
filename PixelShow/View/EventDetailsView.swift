98dd9160ab4444e274c190ee0f8cb3ba131e2c0a
Thaís Fernandes <throdriguesfernandes@gmail.com>
2021-03-31 15:58:27 -0300

    details view done

--- PixelShow/View/EventDetailsView.swift
+++ PixelShow/View/EventDetailsView.swift
@@ -8,18 +8,13 @@
 import SwiftUI
 
 struct EventDetailsView: View {
-    @State private var showingSheet = true
-    let events = listEvents()
-    
+    let event: Event
     
     var body: some View {
         NavigationView {
-            Button("Show Action Sheet") {
-                showingSheet = true
-            }
-            .sheet(isPresented: $showingSheet) {
-                NavigationView {
-                    VStack {
+            ZStack {
+                ScrollView(showsIndicators: false) {
+                    VStack(alignment: .leading) {
                         
                         VStack {
                         }
@@ -28,33 +23,49 @@ struct EventDetailsView: View {
                         .cornerRadius(5.0)
                         .padding(.top)
                         .padding(.bottom, 30)
-                                                
-                        Description(event: events[2])
                         
-                        Text(events[0].textDescription).padding(.vertical)
+                        Description(event: event)
                         
-                        Spacer()
-                    }
-                    .navigationBarTitleDisplayMode(.inline)
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            Image(systemName: "chevron.backward")
-                        }
-                        
-                        ToolbarItem(placement: .principal) {
-                            Text("Desenho de Imprensa")
-                        }
-                    }.padding(.horizontal, 22)
-                }
+                        Text(event.textDescription)
+                            .lineSpacing(1.3)
+                            .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                            .padding(.vertical)
 
-            }
+                        Text("Palestrante nas redes")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        
+                        HStack {
+                            Image("behance")
+                            Image("linkedin")
+                            Image("twitter")
+                            Image("facebook")
+                        }.padding(.bottom, 80)
+                    }
+                }
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Image(systemName: "chevron.backward")
+                    }
+                    
+                    ToolbarItem(placement: .principal) {
+                        Text("Desenho de Imprensa")
+                    }
+                }
+                
+                VStack {
+                    Spacer()
+                    CustomButton(label: "Inscrever-se")
+                }
+            }.padding(.horizontal, 22)
         }
     }
 }
 
 struct EventDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        EventDetailsView()
+        EventDetailsView(event: listEvents()[0])
     }
 }
 
