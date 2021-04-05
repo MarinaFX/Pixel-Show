961b5368d20dadb53e94aa139964893ebc3e8f70
Carolina Nobre <carolinanobre_@hotmail.com>
2021-04-05 16:28:00 -0300

    Adding Content View

---
+++ PixelShow/Components/ContentCard.swift
@@ -0,0 +1,39 @@
+//
+//  ContentCard.swift
+//  PixelShow
+//
+//  Created by Carolina Nobre on 05/04/21.
+//
+
+import SwiftUI
+
+struct ContentCard: View {
+    var content: Content
+    var body: some View {
+        ZStack(alignment:.leading) {
+           content.image
+                .resizable()
+                .frame(width: .infinity, height: 143, alignment: .center)
+                .cornerRadius(4)
+            VStack {
+                Text(content.content.rawValue)
+                    .fontWeight(.medium)
+                    .font(.system(size: 18))
+                    .padding()
+            
+            }
+            .frame(width: 134, height: 38)
+            .background(Color.white)
+           .cornerRadius(4)
+           .offset(x: -4,y: 0)
+            
+            
+        }.padding(EdgeInsets(top: 0, leading: 20, bottom: 16, trailing: 20))
+    }
+}
+
+struct ContentCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentCard(content: Content(content: .ilustração, image: Image("Contents/Ilustracao")))
+    }
+}
